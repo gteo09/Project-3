6e72b8d89b89c9afd63e78155787289c5f89639c
Gabriel Teotonio <gteo09@gmail.com>
2019-05-24 16:04:00 -0700

    built out individual farm routes and map markers

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -9,85 +9,95 @@ class Map extends Component{
     coordinates:[]
   }
 
-    componentDidMount(){
-        this.getAddresses();
-        
-        
-       // this.getCoords(this.state.addresses);      
-    };
+componentDidMount(){
+    this.getAddresses();      
+};
 
-    //loops through db call to get addresses then calls getcoords to get lat, lng values
-    getAddresses = () => {
+//loops through db call to get addresses then calls getcoords to get lat, lng values
+getAddresses = () => {
 
-      API.getInfo().then(res =>{
-        let addressArr = [];
+  API.getInfo().then(res =>{
+    let addressArr = [];
 
-        for(var i =0; i<res.data.length;i++){
+    for(var i =0; i<res.data.length;i++){
 
-          addressArr.push(res.data[i].address)
-        }
-        this.getCoords(addressArr)
-
-      }).catch(err=>console.log(err))
+      addressArr.push(res.data[i].address)
     }
+    this.getCoords(addressArr)
 
-    //gets lat,lng values from geocode api
-    getCoords = async(arr)=>{
+  }).catch(err=>console.log(err))
+};
 
-    const longLatArr = await this.fetchAddress(arr);
+//gets lat,lng values from geocode api
+getCoords = async(arr)=>{
 
-    this.setState({coordinates:longLatArr}) 
-                this.renderMap();
-      //sets state to an array of pairs of lng/lat coordinates then calls render map
-      
-    };
+const longLatArr = await this.fetchAddress(arr);
 
-    fetchAddress = async(arr) => {
+//sets state to an array of pairs of lng/lat coordinates then calls render map
+this.setState({coordinates:longLatArr}) 
+            this.renderMap();
+  
+  
+};
 
-      let longLatArr =[];
+fetchAddress = async(arr) => {
 
-      for(var i=0;i<arr.length;i++){
+  let longLatArr =[];
 
-        let res = await axios.get("https://maps.googleapis.com/maps/api/geocode/json",{
-                params:{
-                    address: arr[i],
-                    key:"AIzaSyB6OceBab84YIQGM0OPCIH89IqydDBckr4"
-                }
-            })
-            // .then((res) => {
-                longLatArr.push({
-                  latitude: res.data.results[0].geometry.location.lat,
-                  longitude: res.data.results[0].geometry.location.lng
-                })
-        }  
-        return longLatArr;
-    }
+  for(var i=0;i<arr.length;i++){
 
-    renderMap=()=>{
-        loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB6OceBab84YIQGM0OPCIH89IqydDBckr4&callback=initMap")
-        window.initMap=this.initMap
-    };
-
-    //initializing map
-     initMap = () => {
-         const myMap = new window.google.maps.Map(document.getElementById('map'), 
-         {
-          center: {lat: 47.6062, lng: -122.3321},
-          zoom: 8
-        });
-
-        //console.log("stateCoords: ",this.state.coordinates)
-
-        this.state.coordinates.map(coordinate => {
-
-          var marker = new window.google.maps.Marker({
-            position: {lat: coordinate.latitude, lng: coordinate.longitude},
-            map: myMap
-          });
+    let res = await axios.get("https://maps.googleapis.com/maps/api/geocode/json",{
+            params:{
+                address: arr[i],
+                key:"AIzaSyB6OceBab84YIQGM0OPCIH89IqydDBckr4"
+            }
         })
+          longLatArr.push({
+            latitude: res.data.results[0].geometry.location.lat,
+            longitude: res.data.results[0].geometry.location.lng
+          })
+    } 
+    return longLatArr;
+}
 
-        return myMap;
-      };
+renderMap=()=>{
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB6OceBab84YIQGM0OPCIH89IqydDBckr4&callback=initMap")
+    window.initMap=this.initMap
+};
+
+//initializing map
+initMap = () => {
+
+    // if longitude and latitude are received from props init map with map centered on prop coordinates
+  if(this.props.lat && this.props.lng){
+
+    const myMap = new window.google.maps.Map(document.getElementById('map'), 
+      {
+      center: {lat: this.props.lat, lng: this.props.lng},
+      zoom: 8
+    });
+      //create marker with coordinates from props
+      var marker = new window.google.maps.Marker({
+        position: {lat: this.props.lat, lng: this.props.lng},
+        map: myMap
+      });
+
+    return myMap;
+    
+  }
+  //else return a google map centered on Seattle
+  else{
+    const myMap = new window.google.maps.Map(document.getElementById('map'), 
+      {
+      center: {lat: 47.6062, lng: -122.3321},
+      zoom: 8
+    });
+
+    return myMap;
+
+  }
+    
+};
 
     render (){
     return (
