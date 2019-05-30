76a55cc2ed19a95cc7e962648f215530a121d53b
jgraffeo <jgraffeo@gmail.com>
2019-05-29 20:26:57 -0700

    tracked

---
+++ client/src/actions/types.js
@@ -0,0 +1,5 @@
+export const GET_ERRORS = "GET_ERRORS";
+export const USER_LOADING = "USER_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const FETCH_USERS = "FETCH_USERS";
+export const FETCH_USER = "FETCH_USER";
